9e8f704e1349c5cac4faa953c65ab934b3105b47
Juan-Sergio Khoshdel <jsergiofm85@gmail.com>
2022-08-15 11:14:15 -0700

    commit tree 6d78c8e97365bc90e89a19faa79df0bea657144e

--- lib/person.rb
+++ lib/person.rb
@@ -1,64 +1,64 @@
 # your code goes here
 class Person
-    attr_reader :name, :happiness, :hygiene
     attr_accessor :bank_account
-
+    attr_reader :name, :happiness, :hygiene
+    
     def initialize(name)
-        @name = name
-        @bank_account = 25
-        @happiness = 8
-        @hygiene = 8
+      @name = name
+      @bank_account = 25
+      @happiness = 8
+      @hygiene = 8
     end
 
     def happiness=(num)
-        @happiness = num.clamp(0, 10)               
+      @happiness = num.clamp(0, 10)
     end
 
     def hygiene=(num)
-        @hygiene = num.clamp(0, 10)
+      @hygiene = num.clamp(0, 10)
     end
 
     def happy?
-        self.happiness > 7
+      self.happiness > 7
     end
 
-    def clean?
-        self.hygiene > 7
+    def clean? 
+      self.hygiene > 7
     end
 
     def get_paid(amount)
-        self.bank_account += amount
-        return "all about the benjamins"
+      self.bank_account += amount
+      return "all about the benjamins"
     end
 
     def take_bath
-        self.hygiene += 4
-        return "♪ Rub-a-dub just relaxing in the tub ♫"
+      self.hygiene += 4
+      return "♪ Rub-a-dub just relaxing in the tub ♫"
     end
 
     def work_out
-        self.hygiene -= 3
-        self.happiness += 2
-        return "♪ another one bites the dust ♫"
+      self.hygiene -= 3
+      self.happiness += 2
+      return "♪ another one bites the dust ♫"
     end
 
     def call_friend(friend)
-        self.happiness += 3
-        friend.happiness += 3
-        return "Hi #{friend.name}! It's #{self.name}. How are you?"
+      self.happiness += 3
+      friend.happiness += 3  
+      return "Hi #{friend.name}! It's #{self.name}. How are you?"
     end
 
-    def start_conversation(person, topic)
-        if topic == "politics"
-            self.happiness -= 2
-            person.happiness -= 2
-            return "blah blah partisan blah lobbyist"
-        elsif topic == "weather"
-            self.happiness += 1
-            person.happiness += 1
-            return "blah blah sun blah rain"
-        else
-            return "blah blah blah blah blah"
-        end
+    def start_conversation(friend, topic)
+      if topic == "politics"
+        self.happiness -= 2
+        friend.happiness -= 2
+        return "blah blah partisan blah lobbyist"
+      elsif topic == "weather"
+        self.happiness += 1
+        friend.happiness += 1
+        return "blah blah sun blah rain"
+      else
+        return "blah blah blah blah blah"
+      end
     end
 end
